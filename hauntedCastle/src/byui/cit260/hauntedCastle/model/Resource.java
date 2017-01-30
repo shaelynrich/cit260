34fbec72d2510c3a209ef1f67c5a9cb7d42645c4
“saratf@gmail.com” <saratf@gmail.com>
2017-01-30 13:22:29 -0700

    01/30/2017 - Sara's part in creating classes for the model layer

--- hauntedCastle/src/byui/cit260/hauntedCastle/model/Resource.java
+++ hauntedCastle/src/byui/cit260/hauntedCastle/model/Resource.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  *
  * @author Saratf
  */
-public class Resource implements Serializable{
+public class Resource extends Scene{
     
     private double amountAvailable;
     private String resourceType;
--- hauntedCastle/src/hauntedcastle/HauntedCastle.java
+++ hauntedCastle/src/hauntedcastle/HauntedCastle.java
@@ -5,8 +5,12 @@
  */
 package hauntedcastle;
 
-import byui.cit260.hauntedCastle.model.Game;
+
 import byui.cit260.hauntedCastle.model.Player;
+import byui.cit260.hauntedCastle.model.Game;
+import byui.cit260.hauntedCastle.model.Map;
+import byui.cit260.hauntedCastle.model.Mouse;
+import byui.cit260.hauntedCastle.model.Resource;
 
 /**
  *
@@ -25,6 +29,26 @@ public class HauntedCastle {
         String playerOneName = playerOne.getName();
         
         System.out.println(playerOne.toString());
+        
+        
+        
+        Game playerGame = new Game();
+        
+        playerGame.setNumbOfPeople(1);
+        playerGame.setTotalTime(10);
+        
+        System.out.println(playerGame.toString());
+        
+        
+        
+        Map mapLocation = new Map();
+        
+        mapLocation.setRowCount(5);
+        mapLocation.setRowCount(6);
+        mapLocation.setHistory("You've been here before");
+        
+       System.out.println(mapLocation);
+        
         
         
     }
