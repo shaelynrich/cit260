d18266c7ae721b7ad572db2ed3c9b3e11c3876f8
“saratf@gmail.com” <saratf@gmail.com>
2017-02-16 13:47:25 -0700

    Merge origin/master
    Conflicts:
    	hauntedCastle/src/hauntedcastle/HauntedCastle.java

--- hauntedCastle/src/byui/cit260/hauntedCastle/control/ComplexEquationsControl.java
+++ hauntedCastle/src/byui/cit260/hauntedCastle/control/ComplexEquationsControl.java
@@ -31,26 +31,23 @@ public class ComplexEquationsControl {
       }
       return false;
 }
-    public double calcVolumeOfPotion (double radius, double height, double volumeOfCylinder, double correctVolumeOfCylinder ){
-        if (radius<0 || height>10){
+    public double calcVolumeOfPotion (double radius, double height, double volumeOfCylinder, double correctVolumeOfCylinder){
+        if (radius<0 || radius>10){
         return -1;
         }
-	if (radius>0 || height<0){
+	if (height>10 || height<0){
         return -1;
-        }
-	if(radius<0 || height>15){
-        return -1;    
         }
         if (volumeOfCylinder <0 && volumeOfCylinder >240){
         return -1;   
         }
         correctVolumeOfCylinder = (Math.PI * Math.pow(radius,2)* height);
-
-	if (volumeOfCylinder == correctVolumeOfCylinder){
-        return correctVolumeOfCylinder;    
+        double roundOff = Math.round(correctVolumeOfCylinder * 100.0) / 100.0;
+        if (volumeOfCylinder == roundOff){
+        return roundOff;    
         }
         else
-        return correctVolumeOfCylinder;    
+        return roundOff;    
 
     }
     }
