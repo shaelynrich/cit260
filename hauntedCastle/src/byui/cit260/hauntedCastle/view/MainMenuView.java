5d8c4706d3d61dcb481d4e6a88f89f7805774d7c
shaelynrich <ric13011@byui.edu>
2017-02-23 13:52:52 -0700

    team assignment 6 opening message

--- hauntedCastle/src/byui/cit260/hauntedCastle/view/MainMenuView.java
+++ hauntedCastle/src/byui/cit260/hauntedCastle/view/MainMenuView.java
@@ -10,9 +10,44 @@ package byui.cit260.hauntedCastle.view;
  * @author Shaelyn
  */
 public class MainMenuView {
+    
+    private String menu;
 
-    void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called***");
+    public MainMenuView() {
+        this.menu = "\n"
+                  + "\n--------------------------------------"
+                  + "\n| Main Menu                           |"
+                  + "\n--------------------------------------"
+                  + "\nN Start New Game"
+                  + "\nG Get and start saved game"
+                  + "\nH Get help on how to play the game"
+                  + "\nS Save game"
+                  + "\nQ Quit"
+                  + "\n--------------------------------------";
     }
+
+    public void displayMainMenuView() {
+        
+        boolean done = false; //set flag to none
+        do {
+            //prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q")) //user wants to quit
+                return; //exit the game
+            
+            //do the requested action and display the next view
+            done = this.doAction(menuOption);
+                
+        }while (!done);
+    }
+
+    private String getMenuOption() {
+        System.out.println("\n*** getMenuOption() function called ***");
+        return "N";
+    }
+
+    private boolean doAction(String menuOption) {
+        System.out.println("\n*** doAction() function called ***");
+        return true;    }
     
 }
